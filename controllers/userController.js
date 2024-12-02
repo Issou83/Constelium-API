110341dc1ccb9a7605192e4613f9bfdc507580cd
Issou83 <issou8@gmail.com>
2024-12-02 20:55:03 +0100

    Validité du Token annulée

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,8 +55,9 @@ exports.login = async (req, res) => {
 
     const token = jwt.sign(
       { _id: user._id },
-      process.env.JWT_SECRET || "defaultSecret",
-      { expiresIn: "1h" }
+      process.env.JWT_SECRET || "defaultSecret"
+      // ,
+      // { expiresIn: "1h" }
     );
     res.status(200).json({ success: true, token });
   } catch (error) {
