b3c82a2ed6c080026c9f98aaeea2642b230b8810
Issou83 <issou8@gmail.com>
2025-01-03 21:37:30 +0100

    Ajout de la verification des cookies pour la protection des routes sensibles

--- server.js
+++ server.js
@@ -1,58 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser"); // Ajoutez ceci pour lire les cookies
 const connectDB = require("./db");
 const nftRoutes = require("./routes/nftRoutes");
+const userRoutes = require("./routes/userRoutes");
 require("dotenv").config();
 
-const userRoutes = require("./routes/userRoutes");
 const app = express();
 const port = 3001;
 
 connectDB();
 
-// Configuration du middleware CORS pour autoriser les requêtes depuis le front-end local
 const corsOptions = {
-  origin: "*",
-  // [
-  //   "https://constelium.netlify.app", // Front-end en ligne
-  //   "http://localhost:3000", // Version locale classique (pour développement)
-  //   "http://localhost:5173", // Serveur local lancé par Vite ou un autre outil
-  //   "http://192.168.1.10", // Adresse IP locale (si nécessaire)
-  //   "http://88.136.135.165:5173", // Adresse IP publique avec le port spécifié pour ton application en local
-  // ]
-  methods: ["GET", "POST", "PUT", "DELETE"], // Méthodes HTTP autorisées
-  allowedHeaders: ["Content-Type", "Authorization"], // En-têtes autorisés
-  credentials: true, // Autoriser les cookies et en-têtes sensibles
+  origin: ["https://constelium.netlify.app", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Répondre aux requêtes préflight (OPTIONS)
-
-// Modification des headers de sécurité
-app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
-  next();
-});
-
+app.use(cookieParser()); // Middleware pour les cookies
 app.use(express.json());
-
-let logs = []; // Variable pour stocker les logs
-
-// Middleware pour capturer les logs
-app.use((req, res, next) => {
-  console.log = function (message) {
-    logs.push(message); // Stocker chaque log
-    process.stdout.write(message + "\n"); // Afficher dans la console
-  };
-  next();
-});
 
 app.use("/user", userRoutes);
 app.use("/nfts", nftRoutes);
-
-// Route pour obtenir les logs
-app.get("/logs", (req, res) => {
-  res.json({ logs });
-});
 
 app.listen(port, () => {
   console.log(`Serveur en écoute sur http://localhost:${port}`);
