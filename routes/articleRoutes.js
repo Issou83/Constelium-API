4132ec26901cdd03db348ce1dc11ca3a4fb40e9d
Issou83 <issou8@gmail.com>
2025-01-27 14:05:45 +0100

    Ajout du middleware d'authentification aux routes des articles

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -6,22 +6,22 @@ const articleController = require("../controllers/articleController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 // CREATE
-router.post("/", articleController.createArticle);
+router.post("/", authMiddleware, articleController.createArticle);
 
 // READ
-router.get("/", articleController.getAllArticles);
+router.get("/", authMiddleware, articleController.getAllArticles);
 
 // READ by ID
-router.get("/:id", articleController.getArticleById);
+router.get("/:id", authMiddleware, articleController.getArticleById);
 
 // UPDATE
-router.put("/:id", articleController.updateArticle);
+router.put("/:id", authMiddleware, articleController.updateArticle);
 
 // DELETE
-router.delete("/:id", articleController.deleteArticle);
+router.delete("/:id", authMiddleware, articleController.deleteArticle);
 
 // PATCH – mise à jour du statut
-router.patch("/:id/status", articleController.toggleStatus);
+router.patch("/:id/status", authMiddleware, articleController.toggleStatus);
 
 // POST – régénérer un article
 // router.post(
