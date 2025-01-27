2413351e7bb7589ba2a4c34f18877872a9f94684
Issou83 <issou8@gmail.com>
2025-01-27 13:19:00 +0100

    Correction de l'importation du middleware d'authentification dans les routes des articles

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const router = express.Router();
 const articleController = require("../controllers/articleController");
-const { authMiddleware } = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 // CREATE
 router.post("/", authMiddleware, articleController.createArticle);
